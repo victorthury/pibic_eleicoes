90e64cb50b62b3a0690eb4ed4515e58bc9e1fcfc
victorthury <41457777+victorthury@users.noreply.github.com>
2019-06-10 12:56:44 -0400

    Update bolsonaro.py

--- bolsonaro.py
+++ bolsonaro.py
@@ -5,10 +5,10 @@ from tweepy.streaming import StreamListener
 import datetime
 fname = "bolsonaro" + datetime.datetime.now().strftime("%d-%m") + ".json"
 
-consumer_key = 'O6fJRuYJBnB4cqfQPf2stHPTd'
-consumer_secret = 'rMS6Hi3IcMyP16QX3Zg15oVmSj1JuR5xcK5SCfqy7xLXmirJ4G'
-access_token = '986070861865279488-BPOVGT90tAR1wMiVPGRHLHvLUA547Fb'
-access_secret = 'AG73nImEpRylglwUMDoy88x1bsblRTj47XAVQvOJc1px3'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
